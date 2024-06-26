6ede318b6de5e5230cb88525b30cec1f00ed299d
hrishi-2407 <hrishibari24@gmail.com>
2024-06-26 22:09:06 +0530

    added main file

---
+++ buddy.py
@@ -0,0 +1,16 @@
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+    command = r.recognize_google(audio)
+
+try:
+    print(command)
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
