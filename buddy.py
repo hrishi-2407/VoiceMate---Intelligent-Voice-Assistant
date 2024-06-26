6fe7c407be5aa6fd86c7a82225e4ec54c146b506
hrishi-2407 <hrishibari24@gmail.com>
2024-06-26 22:32:04 +0530

    created text_to_speech function

--- buddy.py
+++ buddy.py
@@ -8,9 +8,21 @@ with sr.Microphone() as source:
     audio = r.listen(source)
     command = r.recognize_google(audio)
 
-try:
-    print(command)
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+def audio_transcribe():
+    try:
+        print(command)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+import pyttsx3
+
+def text_to_audio(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+audio_transcribe()
+text_to_audio(command)
+
