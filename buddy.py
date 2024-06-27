baa4243843e5f1cbae5d3da4d49ef59f36a5055a
hrishi-2407 <hrishibari24@gmail.com>
2024-06-27 09:42:10 +0530

    adjusted the speech rate and constructed a while loop

--- buddy.py
+++ buddy.py
@@ -1,28 +1,40 @@
 
 import speech_recognition as sr
-
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-    command = r.recognize_google(audio)
-
-def audio_transcribe():
-    try:
-        print(command)
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-
 import pyttsx3
 
-def text_to_audio(command):
-    engine = pyttsx3.init()
+engine = pyttsx3.init() # object creation
+
+def speak(command):
+
+    """ RATE"""
+    rate = engine.getProperty('rate')   # getting details of current speaking rate
+    engine.setProperty('rate', 125)     # setting up new voice rate
+
+    """VOICE"""
+    voices = engine.getProperty('voices')       #getting details of current voice
+    engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
     engine.say(command)
     engine.runAndWait()
 
-audio_transcribe()
-text_to_audio(command)
 
+def listen():
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("-- Say something! --")
+            audio = r.listen(source)
+        try:
+            command = r.recognize_google(audio)
+            if command.lower() == 'exit':
+                break
+            else:
+                print(command)
+                speak(command)
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+if __name__ == '__main__':
+    listen()
