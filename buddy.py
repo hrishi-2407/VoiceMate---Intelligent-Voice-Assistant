9a60acd4e31d5a9f400eda682ff2ea24941c42ae
hrishi-2407 <hrishibari24@gmail.com>
2024-06-27 17:12:52 +0530

    added while loop for continous working

--- buddy.py
+++ buddy.py
@@ -2,7 +2,7 @@
 import speech_recognition as sr
 import pyttsx3
 
-engine = pyttsx3.init() # object creation
+engine = pyttsx3.init() # engine is an object created using pyttsx3.init()
 
 def speak(command):
 
@@ -13,28 +13,31 @@ def speak(command):
     """VOICE"""
     voices = engine.getProperty('voices')       #getting details of current voice
     engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+    # Voice ID: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0, 
+    # Name: Microsoft Zira Desktop - English (United States)
 
     engine.say(command)
     engine.runAndWait()
 
 
 def listen():
-    while True:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        while True:
             print("-- Say something! --")
             audio = r.listen(source)
-        try:
-            command = r.recognize_google(audio)
-            if command.lower() == 'exit':
-                break
-            else:
-                print(command)
-                speak(command)
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
+            try:
+                command = (r.recognize_google(audio))
+                if command.lower() == 'exit':
+                    break
+                else:
+                    print(command)
+                    speak(command)
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results; {0}".format(e))
 
 if __name__ == '__main__':
     listen()
+
